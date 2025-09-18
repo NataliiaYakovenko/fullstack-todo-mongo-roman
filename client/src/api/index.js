a8c47d13ae5a6ed51cef9a383e0fdd787ca427b1
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-09-18 15:58:26 +0300

    Add axios post

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,12 +1,30 @@
 import CONSTANTS from "../constants";
+import axios from "axios";
 
-export const registerUser = (date) => {
-return fetch(`${CONSTANTS.API_BASE}/users/registration`, {
-    method: "POST",
-    headers: {
-      " Content-Type": "application/json",
-    },
-    body: JSON.stringify(date),
-  })
-  .then((response) => response.json());
+// export const registerUser = (data) => {
+// return fetch(`${CONSTANTS.API_BASE}/users/registration`, {
+//     method: "POST",
+//     headers: {
+//       " Content-Type": "application/json",
+//     },
+//     body: JSON.stringify(data),
+//   })
+//   .then((response) => response.json());
+// };
+
+export const registerUser = async (data) => {
+  try {
+    const response = await axios.post(
+      `${CONSTANTS.API_BASE}/users/registration`,
+      data,
+      {
+        headers: {
+          " Content-Type":"application/json",
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
 };
