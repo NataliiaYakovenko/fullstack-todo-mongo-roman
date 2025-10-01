2ae904b87e637f32c05f983d5b38402fc6b558ad
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-10-01 12:51:36 +0300

    Generator function

--- sandbox.js
+++ sandbox.js
@@ -22,3 +22,39 @@ const myIterator = {
     }
   },
 };
+/////////////////////////////////////////////////////////////////////////
+//Функції генератори
+
+function* myGenerator() {
+  console.log("String1");
+  yield "Проміжний результат 1";
+  console.log("String2");
+  yield "Проміжний результат 2";
+  console.log("String3");
+  yield "Проміжний результат 3";
+}
+
+const gen = myGenerator();
+////////////////////////////////////////////////////////////////////////////////
+
+function* numberGenerator(start, end) {
+  for (let i = start; i <= end; i++) {
+    yield i;
+  }
+}
+
+const iterator = numberGenerator(0, 100);
+
+const it = numberGenerator(0, 100);
+for (let i = 0; i <= 100; i++) {
+  console.log(it.next());
+}
+
+const it1 = numberGenerator(0, 100);
+let sum = 0;
+for (let i = 0; i <= 100; i++) {
+  const { value } = it1.next();
+  sum += value;
+}
+
+console.log(sum);
