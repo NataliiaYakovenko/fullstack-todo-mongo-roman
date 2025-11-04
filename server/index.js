5305f01ebf47bd9a8cb0696993c877ba2c50e0eb
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-11-04 12:04:20 +0200

    Add connect websocket

--- server/index.js
+++ server/index.js
@@ -2,15 +2,28 @@ const http = require("http");
 const app = require("./app");
 const { Server } = require("socket.io");
 
-const cors ={
-  origin:'*'
-}
+const cors = {
+  origin: "*",
+};
 
 const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 
 const io = new Server(server, cors);
+
+io.on("connect", (socket) => {
+  console.log("CONNECTION");
+
+  //кожні 5 секунд відправляти на клієнт якесь повідомлення
+  setTimeout(() => {
+    io.emit('NEW NOTIFICATION',{notification:'Something new happening'})
+  }, 5000);
+
+  socket.on("disconnect", (reason) => {
+    console.log(reason);
+  });
+});
 
 server.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
