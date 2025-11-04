a1c224a3030974b5ab48cb134f38173f16649bac
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-11-04 11:33:28 +0200

    Add new Server

--- server/index.js
+++ server/index.js
@@ -10,6 +10,8 @@ const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 
+const io = new Server(server, cors);
+
 server.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
